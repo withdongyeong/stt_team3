86ab97ec4533d7cfba40c085a9a5519285f3b72b
DongyeongKim <withdongyeong@gmail.com>
2021-08-24 10:59:18 +0000

    update basic gui

--- main.py
+++ main.py
@@ -10,6 +10,48 @@ class WindowClass(QMainWindow, form_class) :
         super().__init__()
         self.setupUi(self)
 
+        # 구독 key label
+        self.subscriptionKeyLabel = self.findChild(QLabel, 'subscriptionKeyLabel')
+
+        # 구독 key edit
+        self.subscriptionKeyEdit = self.findChild(QLineEdit, 'subscriptionKeyEdit')
+
+        # endpoint label
+        self.endpointLabel = self.findChild(QLabel, 'endpointLabel')
+
+        # endpoint edit
+        self.endpointEdit = self.findChild(QLineEdit, 'endpointEdit')
+
+        # start 버튼
+        self.startButton = self.findChild(QPushButton, 'startButton')
+        self.startButton.clicked.connect(self.start)
+
+        # stop 버튼
+        self.stopButton = self.findChild(QPushButton, 'stopButton')
+        self.stopButton.clicked.connect(self.stop)
+
+        # soundToTextView
+        self.soundToTextView = self.findChild(QTextEdit, 'soundToTextView')
+
+        # 작업 경로 browse 버튼
+        self.browseButton = self.findChild(QPushButton, 'browseButton')
+        self.browseButton.clicked.connect(self.browse)
+
+        # 작업 경로 label
+        self.browseLabel = self.findChild(QLabel, 'browseLabel')
+
+    # start 버튼 액션
+    def start(self):
+        pass
+
+    # stop 버튼 액션
+    def stop(self):
+        pass
+
+    # browse 버튼 액션
+    def browse(self):
+        pass
+
 if __name__ == "__main__" :
 
     #QApplication : 프로그램을 실행시켜주는 클래스
