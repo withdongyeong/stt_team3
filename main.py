c5d2770121324a164f9bf89cd1a53bd59467917c
DongyeongKim <withdongyeong@gmail.com>
2021-08-24 10:34:30 +0000

    initial empty gui added

---
+++ main.py
@@ -0,0 +1,25 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+# gui 클래스
+form_class = uic.loadUiType("main.ui")[0]
+class WindowClass(QMainWindow, form_class) :
+    def __init__(self) :
+        super().__init__()
+        self.setupUi(self)
+
+if __name__ == "__main__" :
+
+    #QApplication : 프로그램을 실행시켜주는 클래스
+    app = QApplication(sys.argv)
+
+    #WindowClass의 인스턴스 생성
+    myWindow = WindowClass()
+
+    #프로그램 화면을 보여주는 코드
+    myWindow.show()
+
+    #프로그램을 이벤트루프로 진입시키는(프로그램을 작동시키는) 코드
+    app.exec_()
