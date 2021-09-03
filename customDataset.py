94a2830cfa8e1cfb14be6d3791bc31109f39127a
ceaser3484 <ceaser3484@gmail.com>
2021-09-03 15:11:01 +0900

    update train algorhythm and will get result soon

--- customDataset.py
+++ customDataset.py
@@ -1,34 +1,42 @@
 import glob
 import os
-
 from torch.utils.data import Dataset
+from chardet import detect
 
+class TextDataset(Dataset):
 
-class textDataset(Dataset):
-  def __init__(self, data_dir):
-    # 데이터 정의
-    self.all_data = sorted(glob.glob(os.path.join(data_dir, "*.txt")))
+    def __init__(self, data_dir):
+        self.all_data = sorted(glob.glob(os.path.join(data_dir, '*', '*')))
 
-  def __getitem__(self, item):
-    # 데이터 읽기
-    data_path = self.all_data[item]  # 앞서 all_data에 저장된 경로들을 하나씩 data_path에 저장할수있음
-    # 이 경우에는 파일이름.txt가 될것
+    def __getitem__(self, item):
+        data_path = self.all_data[item]
 
-    # 텍스트 읽기
-    f = open(data_path)
-    text = ""
-    # 첫번째 줄은 label
-    # strip() 함수는 문자열의 선행, 후행 개행 문자를 모두 제거
-    label = f.readline().strip()
-    # 두번째 줄 부터는 텍스트에 저장
-    while True:
-      line = f.readline()
-      if not line: break
-      text += line
-    f.close()
+        with open(data_path, 'r', encoding=self.find_codec(data_path)) as file:
+            text = ''
+            label = file.readline().strip()
+            while True:
+                line = file.readline()
+                if not line:
+                    break
+                text += line
 
-    return label, text
+        return label, text
 
-  def __len__(self):
-    length = len(self.all_data)
-    return length
+    def __len__(self):
+        length = len(self.all_data)
+        return length
+
+    def find_codec(self, file_path):
+
+        '''텍스트 마다 인코딩이 다를 수 있으므로 인코딩을 찾아서 읽게 해 주는 함수'''
+
+        # present_path = os.path.join(os.getcwd(), 'stt_team3')
+        # print(file_path)
+
+        # with open(os.path.join(present_path, file_path), 'rb') as file:
+        with open(file_path, 'rb') as file:
+            raw_data = file.read()
+            charset = detect(raw_data)['encoding']
+            return charset
+
+        return None
